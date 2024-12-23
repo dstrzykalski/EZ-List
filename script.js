ba0b9dc859113364d2a5c413a2be0a31a9b18c6f
dstrzykalski <dstrzykalski@yahoo.com>
2024-12-22 23:52:32 -0500

    Movie API Functions

--- script.js
+++ script.js
@@ -5,3 +5,38 @@ const MOVIE_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c7
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 const main = document.getElementById('main')
+
+// Get initial movies
+getMovies(MOVIE_URL)   
+
+async function getMovies(url) {
+    const res = await fetch(url)
+    const data = await res.json()
+
+    showMovies(data.results)
+}
+
+function showMovies(movies) {
+    main.innerHTML = ''
+
+    movies.forEach((movie) => {
+        const { title, poster_path, vote_average, overview } = movie
+
+        const movieEl = document.createElement('div')
+        movieEl.classList.add('movie')
+
+        movieEl.innerHTML = `
+            <img src="${MOVIE_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview}
+            </div>
+        `
+        main.appendChild(movieEl)
+    }
+    )
+}
