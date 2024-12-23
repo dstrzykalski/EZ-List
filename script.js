bf8901b7dd446d5aec3a8d347f1415061dd8402b
dstrzykalski <dstrzykalski@yahoo.com>
2024-12-22 23:48:59 -0500

    Expanded Script

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const MOVIE_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
 const MOVIE_PATH = 'https://image.tmdb.org/t/p/w1280'
 const MOVIE_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
+
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+const main = document.getElementById('main')
